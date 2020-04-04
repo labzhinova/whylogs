92cd9ed598c8f2cf924aafeb83d2c97011b3e315
Andy Dang🤖 <andy@whylabs.ai>
2020-04-03 21:06:46 -0700

    Use clikt library to support parsing arguments (CSV path)

--- src/main/kotlin/ai/whylabs/profiler/core/ProfilerDemo.kt
+++ src/main/kotlin/ai/whylabs/profiler/core/ProfilerDemo.kt
@@ -1,5 +1,8 @@
 package ai.whylabs.profiler.core
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
 import java.io.FileReader
@@ -7,25 +10,37 @@ import java.text.NumberFormat
 import java.util.Locale
 import kotlin.system.measureTimeMillis
 
-internal val NumberFormatter = NumberFormat.getNumberInstance(Locale.US)
+/**
+ * Example test datasets:
+ * * https://www.kaggle.com/sobhanmoosavi/us-accidents
+ */
+class ProfilerDemo : CliktCommand() {
+    private val csvFile: String by option(help = "The CSV file path").prompt("CSV input")
 
-fun main(args: Array<String>) {
-    val initialFreeMemory = Runtime.getRuntime().freeMemory()
-    val datasetProfile = DatasetProfile("testDataset")
-    val executionTimeInMs = measureTimeMillis {
-        // TODO: dynamic file path
-        FileReader("/Users/andy/Downloads/US_Accidents_Dec19.csv").use {
-            val parser = CSVParser(it, CSVFormat.DEFAULT.withFirstRecordAsHeader())
-            for (record in parser) {
-                datasetProfile.track(record.toMap())
+    override fun run() {
+
+        val initialFreeMemory = Runtime.getRuntime().freeMemory()
+        val datasetProfile = DatasetProfile("DemoDataset")
+        val executionTimeInMs = measureTimeMillis {
+            FileReader(csvFile).use {
+                val parser = CSVParser(it, CSVFormat.DEFAULT.withFirstRecordAsHeader())
+                for (record in parser) {
+                    datasetProfile.track(record.toMap())
+                }
             }
+            println(datasetProfile.toJsonString())
         }
-        println(datasetProfile.toJsonString())
-    }
-    // force GC
-    Runtime.getRuntime().gc()
+        // force GC
+        Runtime.getRuntime().gc()
 
-    val usedMemory = initialFreeMemory - Runtime.getRuntime().freeMemory()
-    println("Used memory (bytes): ${NumberFormatter.format(usedMemory)}")
-    println("Execution time (seconds): ${NumberFormatter.format(executionTimeInMs / 1000.0)}")
+        val usedMemory = initialFreeMemory - Runtime.getRuntime().freeMemory()
+        println("Used memory (bytes): ${NumberFormatter.format(usedMemory)}")
+        println("Execution time (seconds): ${NumberFormatter.format(executionTimeInMs / 1000.0)}")
+    }
+
+    companion object {
+        internal val NumberFormatter = NumberFormat.getNumberInstance(Locale.US)
+    }
 }
+
+fun main(args: Array<String>) = ProfilerDemo().main(args)
