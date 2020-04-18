0323c43341d56eca803013149bb3cb519d023211
Andy Dang🤖 <andy@whylabs.ai>
2020-04-18 15:06:06 -0700

    Migrate Utils to Java

--- src/main/kotlin/ai/whylabs/profiler/core/DatasetProfile.kt
+++ src/main/kotlin/ai/whylabs/profiler/core/DatasetProfile.kt
@@ -1,9 +1,12 @@
 package ai.whylabs.profiler.core
 
+import ai.whylabs.profiler.jvm.Utils
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import java.time.Instant
 import java.util.concurrent.ConcurrentHashMap
 
+// a data class that can be rendered by Gson
 data class InterpretableDatasetProfile(
     val name: String?,
     val timestamp: Instant,
@@ -23,23 +26,26 @@ class DatasetProfile(val name: String?, val timestamp: Instant) {
         columns.forEach { (column, value) -> track(column, value) }
     }
 
-    fun toJsonString(): String {
+    fun toInterpretableObject(): InterpretableDatasetProfile {
         val intpColStats = columns.mapValues { (_, column) -> column.toInterpretableStatistics() }
 
-        return Gson.toJson(InterpretableDatasetProfile(name, timestamp, intpColStats))
+        return InterpretableDatasetProfile(name, timestamp, intpColStats)
+    }
+
+    fun toJsonString(): String {
+        return Gson.toJson(this.toInterpretableObject())
     }
 
     companion object {
-        private val Gson = GsonBuilder()
+        val Gson: Gson = GsonBuilder()
             .setPrettyPrinting()
             .serializeSpecialFloatingPointValues()
             .registerTypeAdapter(
-                ByteArray::class.java, ByteArrayToBase64TypeAdapter()
+                ByteArray::class.java, Utils.ByteArrayToBase64TypeAdapter()
             )
             .registerTypeAdapter(
-                Instant::class.java, InstantToLongTypeAdapter()
+                Instant::class.java, Utils.InstantToLongTypeAdapter()
             )
             .create()
-
     }
 }
