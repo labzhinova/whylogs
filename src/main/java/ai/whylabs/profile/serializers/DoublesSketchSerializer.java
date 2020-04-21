607024493dc1b3fb2e9b625b9cad1df530412963
Andy Dang🤖 <andy@whylabs.ai>
2020-04-21 12:46:53 -0700

    Add DoubleSketch serializer

---
+++ src/main/java/ai/whylabs/profile/serializers/DoublesSketchSerializer.java
@@ -0,0 +1,22 @@
+package ai.whylabs.profile.serializers;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.apache.datasketches.memory.Memory;
+import org.apache.datasketches.quantiles.DoublesSketch;
+import org.apache.datasketches.quantiles.UpdateDoublesSketch;
+
+public class DoublesSketchSerializer extends Serializer<DoublesSketch> {
+    @Override
+    public void write(Kryo kryo, Output output, DoublesSketch sketch) {
+        kryo.writeObject(output, sketch.toByteArray());
+    }
+
+    @Override
+    public DoublesSketch read(Kryo kryo, Input input, Class<DoublesSketch> type) {
+        byte[] bytes = kryo.readObject(input, byte[].class);
+        return UpdateDoublesSketch.wrap(Memory.wrap(bytes));
+    }
+}
---
+++ src/test/java/ai/whylabs/profile/serializers/DoublesSketchSerializerTest.java
@@ -0,0 +1,39 @@
+package ai.whylabs.profile.serializers;
+
+import lombok.val;
+import org.apache.datasketches.quantiles.DoublesSketch;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class DoublesSketchSerializerTest extends KryoBaseTest<DoublesSketch> {
+
+    public DoublesSketchSerializerTest() {
+        super(new DoublesSketchSerializer(), DoublesSketch.class);
+    }
+
+    @Override
+    public DoublesSketch createObject() {
+        val sketch = DoublesSketch.builder().setK(256).build();
+        sketch.update(1.0);
+        sketch.update(2.0);
+        return sketch;
+    }
+
+    @Override
+    public void verify(DoublesSketch original, DoublesSketch deserializedObject) {
+        assertEquals(original.getMaxValue(), deserializedObject.getMaxValue());
+        assertEquals(original.getMinValue(), deserializedObject.getMinValue());
+        assertEquals(original.getN(), deserializedObject.getN());
+    }
+
+    @Test
+    public void testRoundtrip() {
+        this.runFullSerialization();
+    }
+
+    @Test
+    public void testNull() {
+        this.runWithNull();
+    }
+}
