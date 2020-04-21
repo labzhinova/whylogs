d657f518230f7bc0db0fe4f2187aea1d185c8ddf
Andy Dang🤖 <andy@whylabs.ai>
2020-04-21 14:34:02 -0700

    Make LongSummary object KryoSerializable

--- src/main/java/ai/whylabs/profile/summary/LongSummary.java
+++ src/main/java/ai/whylabs/profile/summary/LongSummary.java
@@ -1,19 +1,25 @@
 package ai.whylabs.profile.summary;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoSerializable;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+@EqualsAndHashCode
 @AllArgsConstructor
-public class LongSummary {
+public class LongSummary implements KryoSerializable {
 
+  private long min;
+  private long max;
+  private long sum;
   @Getter
   public long count;
-  private long max;
-  private long min;
-  private long sum;
 
   public LongSummary() {
-    this(Long.MIN_VALUE, Long.MAX_VALUE, 0L, 0L);
+    this(Long.MAX_VALUE, Long.MIN_VALUE, 0L, 0L);
   }
 
   public void update(long value) {
@@ -25,5 +31,25 @@ public class LongSummary {
     }
     count++;
     sum += value;
+  }
+
+  @Override
+  public void write(Kryo kryo, Output output) {
+    output.writeLong(count);
+    if (count > 0) {
+      output.writeLong(min);
+      output.writeLong(max);
+      output.writeLong(sum);
+    }
+  }
+
+  @Override
+  public void read(Kryo kryo, Input input) {
+    this.count = input.readLong();
+    if (this.count > 0) {
+      this.min = input.readLong();
+      this.max = input.readLong();
+      this.sum = input.readLong();
+    }
   }
 }
---
+++ src/test/java/ai/whylabs/profile/summary/LongSummaryTest.java
@@ -0,0 +1,33 @@
+package ai.whylabs.profile.summary;
+
+import static org.testng.Assert.assertEquals;
+
+import ai.whylabs.profile.serializers.KryoUtils;
+import com.esotericsoftware.kryo.Kryo;
+import lombok.val;
+import org.testng.annotations.Test;
+
+public class LongSummaryTest {
+
+  @Test
+  public void test_RoundtripSerialization() {
+    val kryo = new Kryo();
+    kryo.register(LongSummary.class);
+
+    val original = new LongSummary(0, 99, 99, 2);
+
+    val roundTripObject = KryoUtils.doRoundTrip(kryo, original, LongSummary.class);
+    assertEquals(original, roundTripObject);
+  }
+
+  @Test
+  public void test_RoundtripSerialization_ZeroCount() {
+    val kryo = new Kryo();
+    kryo.register(LongSummary.class);
+
+    val original = new LongSummary();
+
+    val roundTripObject = KryoUtils.doRoundTrip(kryo, original, LongSummary.class);
+    assertEquals(original, roundTripObject);
+  }
+}
