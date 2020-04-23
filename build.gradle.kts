fa72fd448e157f346ec9af9caaf6dc7d11725371
Andy Dang🤖 <andy@whylabs.ai>
2020-04-23 13:08:07 -0700

    Migrate to Gradle Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,58 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.71"
+    `java-library`
+    id("com.diffplug.gradle.spotless") version ("3.28.1")
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+spotless {
+    java {
+        googleJavaFormat()
+    }
+    format ("misc") {
+        target("**/*.gradle", "**/*.md", "**/.gitignore")
 
+        trimTrailingWhitespace()
+        indentWithSpaces() // or spaces. Takes an integer argument if you don't like 4
+        endWithNewline()
+    }
+}
+
+dependencies {
     implementation("org.apache.datasketches:datasketches-java:1.2.0-incubating")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("org.apache.commons:commons-csv:1.8")
-    implementation("com.github.ajalt:clikt:2.6.0")
-    implementation("com.amazonaws:aws-java-sdk-s3:1.11.758")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    implementation("com.esotericsoftware:kryo:5.0.0-RC5")
+
+    // lombok support
+    compileOnly("org.projectlombok:lombok:1.18.12")
+    annotationProcessor("org.projectlombok:lombok:1.18.12")
+    testCompileOnly("org.projectlombok:lombok:1.18.12")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.12")
+
+    // testng
+    testImplementation("org.testng:testng:6.8")
 }
 
 sourceSets {
     main {
         java.srcDir("src/main/java")
         java.srcDir("src/main/resources")
-        withConvention(KotlinSourceSet::class) { kotlin.srcDir("src/main/kotlin")}
     }
 
     test {
         java.srcDir("src/test/java")
-        java.srcDir("src/test/kotlin")
+        java.srcDir("src/test/java")
     }
 }
-
-
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
     testLogging {
         testLogging.showStandardStreams = true
+        failFast = true
         events("passed", "skipped", "failed")
     }
-}
-
-// config JVM target to 1.8 for kotlin compilation tasks
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
 }
