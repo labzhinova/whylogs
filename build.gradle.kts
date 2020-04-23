e5bfcbab11944b66ceb71928a96f1a863a707a75
Andy Dang🤖 <andy@whylabs.ai>
2020-04-23 13:41:49 -0700

    Initial version of publishing to local maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
     id("com.diffplug.gradle.spotless") version ("3.28.1")
 }
+
+group = "com.whylabs"
+version = "0.1-alpha"
 
 repositories {
     mavenCentral()
@@ -54,5 +59,51 @@ tasks.test {
         testLogging.showStandardStreams = true
         failFast = true
         events("passed", "skipped", "failed")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "whylogs-java"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("WhyLogs")
+                description.set("A lightweight data profiling library")
+                url.set("http://www.whylogs.ai")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("WhyLabs")
+                        name.set("WhyLabs, Inc")
+                        email.set("info@whylabs.ai")
+                    }
+                }
+                scm {
+                    url.set("http://whylabs.ai/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("$buildDir/repos/releases")
+            val snapshotsRepoUrl = uri("$buildDir/repos/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
     }
 }
