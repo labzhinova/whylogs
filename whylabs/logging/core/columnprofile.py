b4649b1993ccd769cba84a6119fe1347934ff0ad
Isaac Backus <isaac@whylabs.ai>
2020-05-12 13:44:22 -0700

    ColumnProfile and DatasetProfile protobuf methods

--- whylabs/logging/core/columnprofile.py
+++ whylabs/logging/core/columnprofile.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 """
 created 5/7/20 by ibackus
-
-TODO:
-    * counter tracker
-    * schema tracker
-    * string tracker
-    * numeric types
-    * multi-threading/parallelism
-    * python version of `TypedDataConverter`
 """
 from whylabs.logging.core.types import TypedDataConverter
 from whylabs.logging.core.statistics import NumberTracker
 from whylabs.logging.core.SummaryConverters import fromNumberTracker
-from whylabs.logging.core.data import ColumnSummary
+from whylabs.logging.core.data import ColumnSummary, ColumnMessage
 
 
 class ColumnProfile:
@@ -22,14 +14,24 @@ class ColumnProfile:
     ----------
     columnName : str
         Name of the column profile
+
+    TODO:
+        * _NUMERIC_TYPES for number type checking
+        * CountersTracker
+        * SchemaTracker
+        * StringTracker
+        * Proper TypedDataConverter type checking
+        * Multi-threading/parallelism
     """
     _NUMERIC_TYPES = None  # TODO: Implement
 
-    def __init__(self, columnName):
+    def __init__(self, columnName, numberTracker=None):
         self.columnName = columnName
         # self.counters = CountersTracker()
         # self.schemaTracker = SchemaTracker()
-        self.numberTracker = NumberTracker()
+        if numberTracker is None:
+            numberTracker = NumberTracker()
+        self.numberTracker = numberTracker
         # self.stringTracker = StringTracker()
 
     def track(self, value):
@@ -53,3 +55,21 @@ class ColumnProfile:
                 if numbersSummary is not None:
                     opts['number_summary'] = numbersSummary
         return ColumnSummary(**opts)
+
+    def toProtobuf(self):
+        return ColumnMessage(
+            name=self.columnName,
+            # counters=self.counters.toProtonbuf(),
+            # schema=self.schemaTracker.toProtobuf(),
+            numbers=self.numberTracker.toProtobuf(),
+            # strings=self.stringTracker.toProtobuf(),
+        )
+
+    @staticmethod
+    def fromProtbuf(message):
+        return ColumnProfile(
+            message.name,
+            # counters=<class>.fromProtobuf(),
+            # schemaTracker=<class>.fromProtobuf(),
+            numberTracker=NumberTracker.fromProtobuf(message.numbers),
+        )
--- whylabs/logging/core/datasetprofile.py
+++ whylabs/logging/core/datasetprofile.py
@@ -3,6 +3,17 @@
 created 5/7/20 by ibackus 
 """
 from whylabs.logging.core import ColumnProfile
+from whylabs.logging.core.iterator import ColumnsChunkSegmentIterator
+from whylabs.logging.core.data import DatasetSummary, DatasetMetadataSegment, \
+    MessageSegment, DatasetProfileMessage
+from uuid import uuid4
+
+
+class _Pair:
+    def __init__(self, column: ColumnProfile):
+        self.name = column.columnName
+        self.statistics = column.toColumnSummary()
+
 
 class DatasetProfile:
     """
@@ -13,13 +24,39 @@ class DatasetProfile:
     timestamp : datetime.datetime
         Timestamp
     """
-    def __init__(self, name, timestamp):
+    # Note: we define the `Pair` here as a "nested" class to mimic the Java
+    # implementation
+    Pair = _Pair
+
+    def __init__(self, name, timestamp, columns=None):
         self.name = name
         self.timestamp = timestamp
-        self.columns = {}
+        if columns is None:
+            columns = {}
+        self.columns = columns
 
-    def track(self, columnName, data):
-        self._trackSingleColumn(columnName, data)
+    @property
+    def timestamp_ms(self):
+        # TODO: Implement proper timestamp conversion
+        return 1588978362910  # Some made up timestamp in milliseconds
+
+    def track(self, columns, data=None):
+        """
+        Parameters
+        ----------
+        columns : str, dict
+            Either the name of a column, or a dictionary specifying column
+            names and the data (value) for each column
+            If a string, `data` must be supplied.  Otherwise, `data` is
+            ignored.
+        data : object, None
+            Value to track.  Specify if `columns` is a string.
+        """
+        if data is not None:
+            self._trackSingleColumn(columns, data)
+        else:
+            for columnName, data in columns.items():
+                self._trackSingleColumn(columnName, data)
 
     def _trackSingleColumn(self, columnName, data):
         try:
@@ -28,3 +65,53 @@ class DatasetProfile:
             prof = ColumnProfile(columnName)
             self.columns[columnName] = prof
         prof.track(data)
+
+    def toSummary(self):
+        column_summaries = {name: colprof.toSummary()
+                            for name, colprof in self.columns.items()}
+        return DatasetSummary(
+            name=self.name,
+            timestamp=self.timestamp_ms,
+            columns=column_summaries
+        )
+
+    def _column_message_iterator(self):
+        for col in self.columns.items():
+            yield col.toProtobuf()
+
+    def toChunkIterator(self):
+        # Generate unique identifier
+        marker = self.name + str(uuid4())
+
+        # Generate metadata
+        metadataSegment = MessageSegment(
+            metadata=DatasetMetadataSegment(
+                name=self.name,
+                timestamp=self.timestamp_ms,
+                marker=self.marker,
+            )
+        )
+
+        yield metadataSegment
+
+        chunkedColumns = self._column_message_iterator()
+        for msg in ColumnsChunkSegmentIterator(chunkedColumns, marker):
+            yield MessageSegment(columns=msg)
+
+    def toProtobuf(self):
+        return DatasetProfileMessage(
+            name=self.name,
+            timestamp=self.timestamp_ms,
+            columns={k: v.toProtobuf() for k, v in self.columns.items()},
+        )
+
+    @staticmethod
+    def fromProtobuf(message):
+        # TODO: convert message timestamp from ms to datetime object
+        dt = message.timestamp
+        return DatasetProfile(
+            message.name,
+            dt,
+            columns={k: ColumnProfile.fromProtbuf(v)
+                     for k, v in message.columns.items()},
+        )
