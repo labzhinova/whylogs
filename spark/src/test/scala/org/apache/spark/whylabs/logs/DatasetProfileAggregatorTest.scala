3ffb253e31019d870a0623623ff04e6be147de0b
Andy Dang🤖 <andy@whylabs.ai>
2020-05-15 21:41:19 -0700

    Add DatasetProfileAggregator.bufferEncoder
    * Add ScalaDatasetProfile case class wrapper for DatasetProfile
    * Add ScalaDatasetProfileUDT as a UserDefinedType
    * Add SharedSparkContext as a trait for Spark testing (turned out to be more complicated than expected)

--- spark/src/test/scala/org/apache/spark/whylabs/logs/DatasetProfileAggregatorTest.scala
+++ spark/src/test/scala/org/apache/spark/whylabs/logs/DatasetProfileAggregatorTest.scala
@@ -3,12 +3,12 @@ package org.apache.spark.whylabs.logs
 import java.time.Instant
 
 import com.whylabs.logging.core.data.InferredType
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.funsuite.AnyFunSuite
 
 case class ExamplePoint(x: Int, y: Double, z: String)
 
-class DatasetProfileAggregatorTest extends SparkTestingSpec {
-  "dataset profile aggregator" should "succeed" in {
+class DatasetProfileAggregatorTest extends AnyFunSuite with SharedSparkContext {
+  test("dataset profile aggregator with select() succeeds") {
     val _spark = spark
     import _spark.implicits._
     val datasetProfileAggregator = DatasetProfileAggregator("test", Instant.now())
@@ -25,7 +25,7 @@ class DatasetProfileAggregatorTest extends SparkTestingSpec {
     val profiles = examples.select(datasetProfileAggregator.toColumn).collect()
     assert(profiles.length == 1)
 
-    val summary = profiles(0).toSummary
+    val summary = profiles(0).value.toSummary
     assert(summary.getColumnsMap.size() == 3)
 
     // assert count
@@ -38,4 +38,50 @@ class DatasetProfileAggregatorTest extends SparkTestingSpec {
     assert(summary.getColumnsMap.get("y").getSchema.getInferredType.getType == InferredType.Type.FRACTIONAL)
     assert(summary.getColumnsMap.get("z").getSchema.getInferredType.getType == InferredType.Type.STRING)
   }
+
+  test("dataset profile aggregator with groupBy().agg() succeeds") {
+    val _spark = spark
+    import _spark.implicits._
+    val datasetProfileAggregator = DatasetProfileAggregator("test", Instant.now())
+    val numberOfEntries = 1000 * 1000
+
+    val examples = (1 to numberOfEntries)
+      // the "x" field value is between 0 to 3 (inclusive)
+      .map(i => ExamplePoint(i % 4, i * 1.50, "text " + i))
+      .toDS()
+      .repartition(32) // repartitioning forces Spark to serialize/deserialize data
+      .toDF() // have to convert to a DataFrame, aka Dataset[Row]
+      .repartition(32)
+
+    import org.apache.spark.sql.functions._
+
+    // group by column x and aggregate
+    val groupedDf = examples.groupBy(col("x"))
+      .agg(datasetProfileAggregator.toColumn.name("whylogs_profile"))
+    groupedDf.printSchema()
+
+    // extract the nested column, collect them and turn them into Summary objects
+    val summaries = groupedDf.select("whylogs_profile.value")
+      .collect()
+      .map(_.getAs[ScalaDatasetProfile](0))
+      .map(_.value)
+      .map(_.toSummary)
+    assert(summaries.length == 4)
+
+    // total number of counts should be the total number of entries
+    assert(summaries.map(_.getColumnsOrThrow("x")).map(_.getCounters.getCount).sum == numberOfEntries)
+    assert(summaries.map(_.getColumnsOrThrow("y")).map(_.getCounters.getCount).sum == numberOfEntries)
+    assert(summaries.map(_.getColumnsOrThrow("z")).map(_.getCounters.getCount).sum == numberOfEntries)
+
+    // verify the max and min in each summary should be the same for the "x" column
+    // remember, we are grouping by x
+    summaries.foreach(s => {
+      val numberSummary = s.getColumnsMap.get("x").getNumberSummary
+      assert(numberSummary.getMax == numberSummary.getMin)
+
+      // assert that each group should have equal number of entries
+      assert(numberSummary.getCount == numberOfEntries / 4)
+    })
+  }
+
 }
